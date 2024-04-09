6d681c9acbdc2489b1a683cca458cfeda983212a
Tiyodor <slenderman2113@gmail.com>
2024-04-09 17:59:58 +0800

    Started working on it

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
+import Home from "./components/Home.vue";
+import { Search } from 'lucide-vue-next';
+import { ShoppingBag } from 'lucide-vue-next';
+import Logo from '../public/GundFactory.png';
 </script>
 
 <template>
-
+  <div class="flex justify-center align-center pt-20 pb-10 mx-10 shadow-lg ">
+    <img class="max-w-[250px]" :src="Logo" />
+    <ul>
+      <il class="m-10 hover:underline active:underline"><a >Home</a></il>
+      <il class="m-10 hover:underline">Shop</il>
+      <il class="m-10 hover:underline">About</il>
+    </ul>
+     <Search class="ml-20 mr-10" />
+     <ShoppingBag />
+  </div>
+  <Home />
 </template>
 
 <style scoped>
-
 </style>
